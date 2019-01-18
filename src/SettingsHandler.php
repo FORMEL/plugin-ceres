6ac4acccd18f9ae3195367ebf4edd259cb6ab1cd
Maximilian Scharf <maximilian.scharf@plentymarkets.com>
2019-01-18 17:17:10 +0100

    load / save for global settings

--- src/SettingsHandler.php
+++ src/SettingsHandler.php
@@ -4,17 +4,25 @@ namespace Ceres;
 
 use Plenty\Modules\Plugin\Models\Plugin;
 use Plenty\Modules\Plugin\Models\Configuration;
+use Plenty\Modules\Plugin\PluginSet\Contracts\PluginSetEntryRepositoryContract;
+use Plenty\Modules\Plugin\PluginSet\Models\PluginSetEntry;
+use Plenty\Modules\Plugin\PluginSet\Repositories\PluginSetEntryRepository;
 
 class SettingsHandler
 {
     public function read($pluginSetId = null, $pluginName = null)
     {
         $plugin = Plugin::where('name', '=', $pluginName)->first();
-
         $pluginId = $plugin->id;
+
+        $pluginSetEntry = PluginSetEntry::where('pluginId', '=', $pluginId)
+            ->where('pluginSetId', '=', $pluginSetId)
+            ->first();
+        $pluginSetEntryId = $pluginSetEntry->id;
 
         $configs = Configuration::where('key', 'like', 'globalSetting:%')
             ->where('plugin_id', '=', $pluginId)
+            ->where('pluginSetEntryId', '=', $pluginSetEntryId)
             ->get();
 
         foreach ($configs as $config)
@@ -29,10 +37,28 @@ class SettingsHandler
     {
         $config = new Configuration();
 
+        $plugin = Plugin::where('name', '=', $pluginName)->first();
+        $pluginId = $plugin->id;
+
+        /** @var PluginSetEntryRepository $pluginSetEntryRepository */
+        $pluginSetEntryRepository = pluginApp(PluginSetEntryRepositoryContract::class);
+
+        $pluginSetEntries = $pluginSetEntryRepository->list();
+
+        $pluginSetEntryId = null;
+
+        foreach($pluginSetEntries as $pluginSetEntry)
+        {
+            if($pluginSetEntry->pluginId == $pluginId)
+            {
+                $pluginSetEntryId = $pluginSetEntry->id;
+            }
+        }
+
         $config->key = 'globalSetting:' . $values[0]['key'];
         $config->value = $values[0]['value'];
-        $config->plugin_id = $pluginSetId;
-        $config->pluginSetEntryId = 4;
+        $config->plugin_id = $pluginId;
+        $config->pluginSetEntryId = $pluginSetEntryId;
 
         $config->save();
     }
